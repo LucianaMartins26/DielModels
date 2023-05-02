cc07b0a9f51967ab0eaee54c92e6a2a4510628a1
Luciana <lucianacmartins8@gmail.com>
2023-05-02 16:00:49 +0100

    [FIX] ignoring Night Reactions issue in CompartmentsCreator class

--- src/diel_models/compartments_creator.py
+++ src/diel_models/compartments_creator.py
@@ -47,20 +47,22 @@ class CompartmentsCreator:
         """
         compartments_copy = copy.deepcopy(self.model.compartments)
         for model_reaction in self.model.reactions:
-            duplicate_reaction = Reaction(id=model_reaction.id.replace("_Day", "_Night"),
-                                          name=model_reaction.name,
-                                          subsystem=model_reaction.subsystem,
-                                          lower_bound=model_reaction.lower_bound,
-                                          upper_bound=model_reaction.upper_bound)
+            if not model_reaction.id.endswith("_Night"):
+                duplicate_reaction = Reaction(id=model_reaction.id.replace("_Day", "_Night"),
+                                              name=model_reaction.name,
+                                              subsystem=model_reaction.subsystem,
+                                              lower_bound=model_reaction.lower_bound,
+                                              upper_bound=model_reaction.upper_bound)
 
-            for model_metabolite in model_reaction.metabolites:
-                duplicate_metabolite = model_metabolite.copy()
-                duplicate_metabolite.id = model_metabolite.id.replace("_Day", "_Night")
-                duplicate_metabolite.name = model_metabolite.name.replace("Day", "Night")
-                duplicate_metabolite.compartment = model_metabolite.compartment.replace("_Day", "_Night")
-                duplicate_reaction.add_metabolites({duplicate_metabolite: model_reaction.metabolites[model_metabolite]})
+                for model_metabolite in model_reaction.metabolites:
+                    duplicate_metabolite = model_metabolite.copy()
+                    duplicate_metabolite.id = model_metabolite.id.replace("_Day", "_Night")
+                    duplicate_metabolite.name = model_metabolite.name.replace("Day", "Night")
+                    duplicate_metabolite.compartment = model_metabolite.compartment.replace("_Day", "_Night")
+                    duplicate_reaction.add_metabolites({duplicate_metabolite: model_reaction.metabolites[model_metabolite]})
 
-            self.model.add_reactions([duplicate_reaction])
+                if not self.model.reactions.has_id(duplicate_reaction.id):
+                    self.model.add_reactions([duplicate_reaction])
 
         for compartment_id, compartment_description in compartments_copy.items():
             self.model._compartments[compartment_id.replace("_Day", "_Night")] = compartment_description.replace("Day", "Night")
