69ccdeb0b5b25394184455af681ca5e9f629a24a
Luciana <lucianacmartins8@gmail.com>
2023-05-26 19:07:40 +0100

    [ADD] drafts to analyse fluxes of biosynthesis reactions + [FIX] code in nitrate_uptake_ratio class

--- examples/simulation_sp.py
+++ examples/simulation_sp.py
@@ -20,29 +20,33 @@ def load_model():
     model.reactions.get_by_id("Ex5_Day").bounds = (0, 1000)
     return model
 
+
 def simulate(model):
     solution = pfba(model).fluxes
-    fva_solution = fva(model, [storage for storage in model.reactions if "Day_sp" in storage.id], fraction_of_optimum=1.0, processes=os.cpu_count())
+    fva_solution = fva(model, [storage for storage in model.reactions if "Day_sp" in storage.id],
+                       fraction_of_optimum=1.0, processes=os.cpu_count())
     assert solution['Biomass_Total'] == 0.11
-    assert round(solution['Ex4_Day']*2, 4) == round(solution['Ex4_Night']*3, 4)
+    assert round(solution['Ex4_Day'] * 2, 4) == round(solution['Ex4_Night'] * 3, 4)
     return solution, fva_solution
-
 
 
 def analyze_solution(pfba_solution, fva_solution):
     produced_at_day = pfba_solution.loc[(pfba_solution.index.str.contains('Day_sp')) & (round(pfba_solution, 5) > 0)]
     produced_at_night = pfba_solution.loc[(pfba_solution.index.str.contains('Day_sp')) & (round(pfba_solution, 5) < 0)]
-    produced_at_day = pd.concat([produced_at_day, fva_solution.loc[produced_at_day.index, ['minimum','maximum']]], axis=1)
-    produced_at_night = pd.concat([produced_at_night, fva_solution.loc[produced_at_night.index, ['minimum','maximum']]], axis=1)
+    produced_at_day = pd.concat([produced_at_day, fva_solution.loc[produced_at_day.index, ['minimum', 'maximum']]],
+                                axis=1)
+    produced_at_night = pd.concat(
+        [produced_at_night, fva_solution.loc[produced_at_night.index, ['minimum', 'maximum']]], axis=1)
     ### add column called robust, that is true if both minimum and maximum have the same sign
-    produced_at_day['robust'] = produced_at_day.apply(lambda x: True if x['minimum'] * x['maximum'] > 0 else False, axis=1)
+    produced_at_day['robust'] = produced_at_day.apply(lambda x: True if x['minimum'] * x['maximum'] > 0 else False,
+                                                      axis=1)
     ### same for night
-    produced_at_night['robust'] = produced_at_night.apply(lambda x: True if x['minimum'] * x['maximum'] > 0 else False, axis=1)
+    produced_at_night['robust'] = produced_at_night.apply(lambda x: True if x['minimum'] * x['maximum'] > 0 else False,
+                                                          axis=1)
     produced_at_day.to_excel(r'produced_at_day.xlsx')
     produced_at_night.to_excel(r'produced_at_night.xlsx')
     print(produced_at_day)
     print(produced_at_night)
-
 
 
 if __name__ == '__main__':
--- src/diel_models/nitrate_uptake_ratio.py
+++ src/diel_models/nitrate_uptake_ratio.py
@@ -35,9 +35,8 @@ class NitrateUptakeRatioCalibrator(Step):
             raise ValueError("id_nitrate_uptake_reaction_night not present in the model that was given.")
 
         same_flux = self.model.problem.Constraint(
-            nitrate_uptake_reaction_day.flux_expression -
-            nitrate_uptake_reaction_night.flux_expression * (3/2), lb=0,
-            ub=0)
+            nitrate_uptake_reaction_day.flux_expression * 2 -
+            nitrate_uptake_reaction_night.flux_expression * 3, lb=0, ub = 0)
         self.model.add_cons_vars(same_flux)
 
     def run(self) -> Model:
